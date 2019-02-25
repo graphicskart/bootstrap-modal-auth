f8d3471fefc9042bfb6c6bc90307395420e3819c
graphicskart <graphicskart2018@gmail.com>
2019-02-25 22:16:26 +0530

    Updates

---
+++ precache-manifest.81b9ec46f27f7bf98a37ec4d8f039af3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f43c900e804634721d45d1a1c6b1bf1",
+    "url": "/bootstrap-modal-auth/static/media/google.7f43c900.png"
+  },
+  {
+    "revision": "20d62caf2d6a1e72bd55",
+    "url": "/bootstrap-modal-auth/static/js/runtime~main.20d62caf.js"
+  },
+  {
+    "revision": "00520cd116fabf66948d",
+    "url": "/bootstrap-modal-auth/static/js/main.00520cd1.chunk.js"
+  },
+  {
+    "revision": "f11f34429dfe23e618aa",
+    "url": "/bootstrap-modal-auth/static/js/2.f11f3442.chunk.js"
+  },
+  {
+    "revision": "00520cd116fabf66948d",
+    "url": "/bootstrap-modal-auth/static/css/main.d08621b5.chunk.css"
+  },
+  {
+    "revision": "5f3e62cb24e5e394aaa5441ba6226730",
+    "url": "/bootstrap-modal-auth/index.html"
+  }
+];
